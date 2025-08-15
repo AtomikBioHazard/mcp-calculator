c846cdb2c389e95850365042802820113d435a11
AtomikBioHazard <atomikbiohazard@gmail.com>
2025-08-15 15:55:13 -0700

    Feat: Create Server
    Implement basic server

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+// Create an MCP Server
+const server = new McpServer({
+  name: "Calculator MCP Server",
+  version: "1.0.0",
+});
